53ec1fafbafbb6ed46e200bf36b81f869a189027
editsmw090-cpu <editsmw090@gmail.com>
2026-09-12 23:55:56 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/space/shootinggame/MainActivity.java
@@ -0,0 +1,40 @@
+package com.space.shootinggame;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl(
+            "https://editsmw090-cpu.github.io/space-shooting-game/"
+        );
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
